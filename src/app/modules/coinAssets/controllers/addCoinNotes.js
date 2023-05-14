a3786cffebfc2670d00790e411139a07e182f433
Adjaro Ogaga <ogagaadjaro@gmail.com>
2023-05-14 22:14:14 +0100

    Fix-Reworked coin note feature

--- src/app/modules/coinAssets/controllers/addCoinNotes.js
+++ src/app/modules/coinAssets/controllers/addCoinNotes.js
@@ -7,23 +7,6 @@ const CoinNotesService = require("../services/notes.services");
 
 exports.createNote = async (req, res, next) => {
   try {
-    // check if research exist
-    const research = await new ResearchService().findAResearch({
-      community_id: req.query.research_community_id,
-    });
-    if (!research) {
-      return next(
-        createError(HTTP.OK, [
-          {
-            status: RESPONSE.SUCCESS,
-            message: "Research Does Not Exist",
-            statusCode: HTTP.Ok,
-            data: {},
-            code: HTTP.Ok,
-          },
-        ])
-      );
-    } else {
     //   //  check if note exist for coin
     // const coinNoteExists = await new CoinNotesService().findARecord({user_id: req.user_id, community_id: research.community_id});
     // if(coinNoteExists){
@@ -41,14 +24,13 @@ exports.createNote = async (req, res, next) => {
     // }
       const dataToCoinNoteModel = {
         user_id: req.user.user_id,
-        research_id: research._id,
-        community_id: research.community_id,
+        coin_id: req.body.coin_id,
         note_text: req.body.note_text,
       };
       const newNote = await new CoinNotesService().create(dataToCoinNoteModel);
 
       return createResponse("Coin Note Created", newNote)(res, HTTP.OK);
-    }
+    
   } catch (err) {
     console.log(err);
 
--- src/app/vallidators/CoinAssets/getNote.validator.js
+++ src/app/vallidators/CoinAssets/getNote.validator.js
@@ -4,6 +4,6 @@ Joi.objectId = require("joi-objectid")(Joi);
 exports.coinNotesQuerySchema = Joi.object({
   page: Joi.number().positive().optional(),
   limit: Joi.number().positive().optional(),
-  research_community_id: Joi.objectId().required(),
+  coin_id: Joi.string().required()
 });
 
