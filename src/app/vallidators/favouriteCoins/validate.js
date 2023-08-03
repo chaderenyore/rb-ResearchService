32ef5beee69af20f63c7e8e549dc64942afef61f
ADJARO OGAGA <45229900+ogagaCodes@users.noreply.github.com>
2023-08-03 10:47:32 +0100

    Update validate.js

--- src/app/vallidators/favouriteCoins/validate.js
+++ src/app/vallidators/favouriteCoins/validate.js
@@ -2,7 +2,6 @@ const Joi = require("joi");
 Joi.objectId = require("joi-objectid")(Joi);
 
 exports.validateFavSchema = Joi.object({
-  user_id: Joi.objectId().required(),
   asset_id: Joi.string().required(),
   coin_name: Joi.string().required(),
 });
